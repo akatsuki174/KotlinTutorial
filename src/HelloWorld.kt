1293391e0e558f0674b9b15efce2cb23eb1be963
akatsuki174 <dev2015js@gmail.com>
2017-04-10 23:39:50 +0900

    SimplestVersionの実装を追加

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,3 +1,11 @@
 /**
  * Created by akatsuki174 on 2017/04/10.
  */
+
+/**
+ * 最もシンプルなHello World.
+ * 文末にセミコロンを付けても付けなくてもいい
+ */
+internal fun SimplestVersion() {
+    println("Hello, World!")
+}
--- src/app.kt
+++ src/app.kt
@@ -2,5 +2,6 @@
  * Created by akatsuki174 on 2017/04/08.
  */
 fun main(args: Array<String>) {
-    println("Hello, World!")
+    // Hello World
+    SimplestVersion()
 }
