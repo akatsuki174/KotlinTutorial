5153b36ff4225d9aea1905e2e0ed80b2eb70cc44
akatsuki174 <dev2015js@gmail.com>
2017-04-11 00:10:46 +0900

    Reading a name from the command lineの実装を追加

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -9,3 +9,15 @@
 internal fun SimplestVersion() {
     println("Hello, World!")
 }
+
+/**
+ * 文字テンプレートや配列へのアクセスの仕方がわかる
+ */
+
+internal fun ReadingANameFromTheCommandLine(args: Array<String>) {
+    if (args.size == 0) {
+        println("Please provide a name as a command-line argument")
+        return
+    }
+    println("Hello, ${args[0]}!")
+}
--- src/app.kt
+++ src/app.kt
@@ -4,4 +4,5 @@
 fun main(args: Array<String>) {
     // Hello World
     SimplestVersion()
+    ReadingANameFromTheCommandLine(args)
 }
