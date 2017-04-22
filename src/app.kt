be270275ca8cf8b82102a1bf532e6c0fc9ae3098
akatsuki174 <dev2015js@gmail.com>
2017-04-23 01:23:51 +0900

    空行追加

--- src/app.kt
+++ src/app.kt
@@ -8,22 +8,30 @@ fun main(args: Array<String>) {
     readingManyNamesFromTheCommandLine(args)
     aMultiLanguageHello(args)
     AnObjectOrientedHello(args[0]).greet()
+
+
     // Basic syntax walk-through
 
     // Use a conditional expressions
     println(max(args[0].toInt(), args[1].toInt()))
     println(max(a = args[0].toInt(), b = args[1].toInt())) // 名前付き引数を使っても書ける
+
     // Null-checks
     nullChecks(args)
+
     // is-checks and smart casts
     println(isChecksAndSmartCasts("aaa"))
     println(isChecksAndSmartCasts(1))
+
     // Use a while-loop
     useAWhileLoop(args)
+
     // Use a for-loop
     useAForLoop(args)
+
     // Use ranges and in
     useRangesAndIn(args)
+    
     // Use when
     usewhen("Hello")
     usewhen(1)
