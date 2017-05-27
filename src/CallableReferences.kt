cf0e50aff366fff150965a55d075688db566e38f
akatsuki174 <dev2015js@gmail.com>
2017-05-27 23:18:08 +0900

    関数オブジェクトに関する例を追加

--- src/CallableReferences.kt
+++ src/CallableReferences.kt
@@ -6,14 +6,22 @@
  * "Callable References"もしくは"Feature Literals"は
  * 名前付きの関数やプロパティを値として渡すことができます。
  * 関数を渡す場合はプレフィックスとして`::`を付けます。
+ * ::を付けることでその関数の関数オブジェクトを取得することができます。
  */
 
 fun referenceToAFunction() {
     val numbers = listOf(1, 2, 3)
     println(numbers.filter(::isOdd))
+
+    println(::square) // 「fun square(kotlin.Int): kotlin.Int」と表示される
+    // println(::square(2)) <- これはエラーになる
+    val funcObj = ::square
+    println(funcObj(2)) // <- これは4が出力される
 }
 
 fun isOdd(x: Int) = x % 2 != 0
+
+fun square(i: Int): Int = i * i
 
 /**
  * 高階関数はcompose(f, g) = f(g(*))のように渡された2つの関数を返します。
